2e6cb62ca595bbe0aef015f6c93923f9227c9388
Edujesseyang <edujesseyang@gmail.com>
2024-05-01 22:02:47 -0700

    asd

--- fetch_and_ajax/script.js
+++ fetch_and_ajax/script.js
@@ -38,18 +38,16 @@ btn.addEventListener("click", function () {
     const link = "http://worldtimeapi.org/api/timezone/America/Los_Angeles";
     fetch(link)
         .then(response => {
-            console.log(response);
             return response.json();
         })
         .then(data => {
-            console.log(data);
+            // get raw data
             const content = document.querySelector('#content');
             content.textContent ="Raw data: " + data.datetime;
             let weekDay = data.day_of_week;
             const day = document.querySelector('#day');
             day.textContent = showWeekDay(weekDay);
-            box.append(content);
-            box.append(day);
+        
 
             // formate time and date.
             let date_time = data.datetime.split('T');
@@ -57,13 +55,11 @@ btn.addEventListener("click", function () {
             date_time.shift();
             let time_arr = date_time[0].split('.');
             let time = time_arr[0];
-
             const timeOutput = document.querySelector("#timeOutput");
             const dateOutput = document.querySelector("#dateOutput");
             timeOutput.textContent = "Time: " + time;
             dateOutput.textContent = "Date: " + date;
-            box.append(timeOutput);
-            box.append(dateOutput);
+          
         })
         .catch(error => {
             console.log("Error: ", error);
