2595faeb8a4a75e43863a94646a3628d8ecb6f81
Edujesseyang <edujesseyang@gmail.com>
2024-05-20 21:34:57 -0700

    add

--- node_js_basic/index.js
+++ node_js_basic/index.js
@@ -1,14 +1,28 @@
-(function () {
-    console.log(`Let's do it:\n`);
-
-    for (let i = 0; i < 10; i++) {
-        for (let j = 0; j < i; j++) {
-            console.log(j);
-        }
-        console.log(i);
-    }
-})()
-
+let funcObj = {
+    func_1: () => {
+        console.log(`Hello`);
+    },
+    func_2: (name = `Jesse`) => {
+        console.log(`, `, name);
+    },
+    func_3: `There is something.`
+}
 
 
+funcObj.func_1();
+funcObj.func_2(`Tom`);
+console.log(funcObj.func_3);
+let arr = [
+    { name: `jesse`, age: 32 },
+    { name: `chloe`, age: 31 }
+]
 
+arr.forEach((that) => {
+    console.log(`name is `, that.name, ` age is `);
+    console.log(that.age)
+});
+
+for (let i = 0; i < arr.length; i++) {
+    console.log(`name is `, arr[i].name, ` age is `);
+    console.log(arr[i].age)
+}
