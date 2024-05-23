1014ffb380320b6b78c6173c33c8c0f91dfb833d
Edujesseyang <edujesseyang@gmail.com>
2024-05-22 20:02:07 -0700

    add

--- node_js_basic/destructuring.js
+++ node_js_basic/destructuring.js
@@ -9,3 +9,12 @@ const { a: my_a, b: my_b, c: my_c } = obj;
 console.log(my_a);
 console.log(my_b);
 console.log(my_c);
+
+const arr = [1, 2, 3, 4, 5];
+const [first, second, third, four, five] = arr;
+
+console.log(first);
+console.log(second);
+console.log(third);
+console.log(four);
+console.log(five);
