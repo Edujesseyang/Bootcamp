7c82b100adb095f5d0d30cb0fbb7e130cad33058
Edujesseyang <edujesseyang@gmail.com>
2024-05-23 18:55:19 -0700

    add

--- node_js_basic/file_stream.js
+++ node_js_basic/file_stream.js
@@ -1,12 +1,26 @@
+// part 1 : get command line argument in to a string.
 let my_arr = [];
-for(let i = 2; i < process.argv.length; i++){
+for (let i = 2; i < process.argv.length; i++) {
     my_arr.push(process.argv[i]);
 }
-
 let my_str = my_arr.join(' ');
 console.log(my_str);
 
+
+
+// part 2 : append the string into the target file.
 const fs = require('fs');
-fs.writeFile(`fileName.txt`, my_str, `utf-8`, (err) => {
-    err ? console.log(err) : console.log(`Success`);
-})
+const fileAppend = (content) => {
+    fs.appendFile(`fileName.txt`, content + ` \n`, `utf-8`, (err) => {
+        err ? console.log(err) : console.log(`Success`);
+    })
+}
+
+fileAppend(my_str);
+
+
+// writeFile can replace the appendFile if need to replace the data every time calling the prog
+// const fs = require('fs');
+// fs.writeFile(`fileName.txt`, my_str, `utf-8`, (err) => {
+//     err ? console.log(err) : console.log(`Success`);
+// })
